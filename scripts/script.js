12f45e4e51aee47a5dd350e3fb241a2b32665f19
MiraivDunFD <94361815+MiraivDunFD@users.noreply.github.com>
2022-07-03 21:38:54 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,9 @@
 // JavaScript Document
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
+hamburger.addEventListener("click",()=> {
+ hamburger.classList.toggle("active");
+ navMenu.classList.toggle("active");
+
+})
