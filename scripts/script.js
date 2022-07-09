057b331fa42985a9e2152774d1f545a6073c04c1
MiraivDunFD <94361815+MiraivDunFD@users.noreply.github.com>
2022-07-09 22:48:33 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,15 @@
 // JavaScript Document
+
+// Hamburger menu aanspreken met juiste classes.
+// class .hamburger is de menu icoon met 3 streepjes.
+// class .nav-menu is het blok waarin de menu items zitten.
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
+// Wanneer je klikt op de class .hamburger (3 streepjes icoon).
+// 1. gaat de css animatie die aan de class .hamburger.active (const hamburger) is toegevoegd afspelen (3 streepjes wordt een kruisje).
+// 2. menu items die in class .nav-menu.active (const navMenu) staan worden weergegeven.
 hamburger.addEventListener("click",()=> {
  hamburger.classList.toggle("active");
  navMenu.classList.toggle("active");
-
 })
